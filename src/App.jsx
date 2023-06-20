c305d9dc3ffdcfd8355c15e9b135fe345d40aff6
Mzwandile <mzwandilethatojunior@gmail.com>
2023-06-20 14:12:02 +0200

    project update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 const App = () => {
+  let [count, setCount] = useState(0);
+
+  function decreaseCount() {
+    setCount(count - 1);
+  }
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+
+  function resetCount() {
+    setCount((count = 0));
+  }
+
   return (
     <>
-      <div></div>
+      <div>
+        <button onClick={decreaseCount} className="border-4 p-3">
+          {" "}
+          -{" "}
+        </button>
+        <span className=" p-3">{count}</span>
+        <button onClick={increaseCount} className="border-4 p-3">
+          {" "}
+          +{" "}
+        </button>
+        <button onClick={resetCount} className="border-4 p-3">
+          Reset
+        </button>
+      </div>
     </>
   );
 };
